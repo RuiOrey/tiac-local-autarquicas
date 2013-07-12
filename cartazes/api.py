fb3ea8853530558c9eb58bd77aab422bfadc08e2
RuiOrey <rui.albuquerque.dorey@gmail.com>
2013-07-12 13:51:45 +0100

    Inicializando webservices por Tastypie para a aplicação Candidatura

---
+++ cartazes/api.py
@@ -0,0 +1,4 @@
+#ficheiro que vai ter a API TastyPie segundo este video : http://www.youtube.com/watch?v=FtREKCAWqBk
+from tastypie.resources import ModelResource
+from tastypie.constants import ALL
+from models import Registofoto
